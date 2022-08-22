dfd2c35639b1ecbfb35c2bb822ba122d499b07bc
Orlando Montes <omontes@indelpro.com>
2022-08-22 11:40:55 -0500

    cree el archivo para las rutas, y se pueden acceder a ella desde el sidebar. Tambien hice que se activara una clase de un elemento html basado en el routerlink

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html',
-  styleUrls: ['./por-pais.component.css']
+  templateUrl: './por-pais.component.html'
 })
 export class PorPaisComponent implements OnInit {
 
